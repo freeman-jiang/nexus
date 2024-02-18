2e71a534f75279a5378f3707185e56010147f2e5
Leo Huang <leogjhuang@gmail.com>
2024-02-17 16:18:18 -0800

    add sample data

--- graph/index.js
+++ graph/index.js
@@ -20,11 +20,9 @@ function showNodeDetails(node) {
 function getGraphFromQueryString(query) {
   var graphGenerators = require("ngraph.generators");
   var createGraph = graphGenerators[query.graph] || graphGenerators.grid;
-  return createGraph(
-    getNumber(query.n),
-    getNumber(query.m),
-    getNumber(query.k)
-  );
+  return query.graph
+    ? createGraph(getNumber(query.n), getNumber(query.m), getNumber(query.k))
+    : populateGraph();
 }
 
 function getNumber(string, defaultValue) {
@@ -32,4 +30,41 @@ function getNumber(string, defaultValue) {
   return typeof number === "number" && !isNaN(number)
     ? number
     : defaultValue || 10;
+}
+
+function populateGraph() {
+  var nodes = [
+    { id: 1, label: "Node 1" },
+    { id: 2, label: "Node 2" },
+    { id: 3, label: "Node 3" },
+    { id: 4, label: "Node 4" },
+    { id: 5, label: "Node 5" },
+    { id: 6, label: "Node 6" },
+    { id: 7, label: "Node 7" },
+    { id: 8, label: "Node 8" },
+    { id: 9, label: "Node 9" },
+  ];
+
+  var links = [
+    { source: 1, target: 2 },
+    { source: 1, target: 3 },
+    { source: 1, target: 4 },
+    { source: 1, target: 5 },
+    { source: 1, target: 6 },
+    { source: 1, target: 7 },
+    { source: 1, target: 8 },
+    { source: 1, target: 9 },
+  ];
+
+  var createGraph = require("ngraph.graph");
+  var g = createGraph();
+
+  nodes.forEach(function (node) {
+    g.addNode(node.id, node);
+  });
+  links.forEach(function (link) {
+    g.addLink(link.source, link.target);
+  });
+
+  return g;
 }
