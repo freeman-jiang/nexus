2a266153784827de16cf340da99cedda3c00630c
Leo Huang <leogjhuang@gmail.com>
2024-02-17 17:59:30 -0800

    move sample data to data.js

---
+++ graph/data.js
@@ -0,0 +1,90 @@
+const nodes = [
+  {
+    id: 1,
+    data: {
+      name: "John Doe",
+      interests: ["AI", "Blockchain"],
+      school: "University of Waterloo",
+    },
+  },
+  {
+    id: 2,
+    data: {
+      name: "Jane Doe",
+      interests: ["Backend"],
+      school: "Stanford University",
+    },
+  },
+  {
+    id: 3,
+    data: {
+      name: "James Doe",
+      interests: ["iOS", "Frontend"],
+      school: "MIT",
+    },
+  },
+  {
+    id: 4,
+    data: {
+      name: "Jill Doe",
+      interests: ["Design", "Entrepreneurship"],
+      school: "UC Berkeley",
+    },
+  },
+  {
+    id: 5,
+    data: {
+      name: "Jack Doe",
+      interests: ["Data Science"],
+      school: "Stanford University",
+    },
+  },
+  {
+    id: 6,
+    data: {
+      name: "Jenny Doe",
+      interests: ["Design", "Frontend"],
+      school: "University of Waterloo",
+    },
+  },
+  {
+    id: 7,
+    data: {
+      name: "Jared Doe",
+      interests: ["Backend", "Blockchain"],
+      school: "UC Berkeley",
+    },
+  },
+  {
+    id: 8,
+    data: {
+      name: "Jasmine Doe",
+      interests: ["iOS", "Data Science"],
+      school: "MIT",
+    },
+  },
+  {
+    id: 9,
+    data: {
+      name: "Jasper Doe",
+      interests: ["AI", "Entrepreneurship"],
+      school: "University of Waterloo",
+    },
+  },
+];
+
+const links = [
+  { source: 1, target: 2 },
+  { source: 1, target: 3 },
+  { source: 1, target: 4 },
+  { source: 1, target: 5 },
+  { source: 1, target: 6 },
+  { source: 1, target: 7 },
+  { source: 1, target: 8 },
+  { source: 1, target: 9 },
+];
+
+module.exports = {
+  nodes,
+  links,
+};
--- graph/index.js
+++ graph/index.js
@@ -33,34 +33,14 @@ function getNumber(string, defaultValue) {
 }
 
 function populateGraph() {
-  var nodes = [
-    { id: 1, label: "Node 1" },
-    { id: 2, label: "Node 2" },
-    { id: 3, label: "Node 3" },
-    { id: 4, label: "Node 4" },
-    { id: 5, label: "Node 5" },
-    { id: 6, label: "Node 6" },
-    { id: 7, label: "Node 7" },
-    { id: 8, label: "Node 8" },
-    { id: 9, label: "Node 9" },
-  ];
-
-  var links = [
-    { source: 1, target: 2 },
-    { source: 1, target: 3 },
-    { source: 1, target: 4 },
-    { source: 1, target: 5 },
-    { source: 1, target: 6 },
-    { source: 1, target: 7 },
-    { source: 1, target: 8 },
-    { source: 1, target: 9 },
-  ];
-
   var createGraph = require("ngraph.graph");
   var g = createGraph();
 
+  var nodes = require("./data.js").nodes;
+  var links = require("./data.js").links;
+
   nodes.forEach(function (node) {
-    g.addNode(node.id, node);
+    g.addNode(node.id, node.data);
   });
   links.forEach(function (link) {
     g.addLink(link.source, link.target);
